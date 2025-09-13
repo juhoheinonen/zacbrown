02e87f3537f597e26fd6f8049cdf6b9e728f7a7c
Juho Antti Heinonen <juho.antti.heinonen@gmail.com>
2025-09-13 16:20:13 +0300

    Improve drawings. Add standing brownie

--- zacbrown.c
+++ zacbrown.c
@@ -14,10 +14,14 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "Zachary Brownie - sprite animation");
 
     // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
-    Texture2D scarfy = LoadTexture("img/scarfy.png"); // Texture loading
+    Texture2D brownie_standing = LoadTexture("img/brownie.png"); // Texture loading
+    Texture2D brownie_running = LoadTexture("img/brownie_running.png"); // Texture loading
+
+    Vector2 position_standing = {350.0f, 150.0f};
+    Rectangle static_rec = {0.0f, 0.0f, (float)brownie_standing.width, (float)brownie_standing.height};
 
     Vector2 position = {350.0f, 280.0f};
-    Rectangle frameRec = {0.0f, 0.0f, (float)scarfy.width / 6, (float)scarfy.height};
+    Rectangle frameRec = {0.0f, 0.0f, (float)brownie_running.width / 6, (float)brownie_running.height};
     int currentFrame = 0;
 
     int framesCounter = 0;
@@ -41,7 +45,7 @@ int main(void)
             if (currentFrame > 5)
                 currentFrame = 0;
 
-            frameRec.x = (float)currentFrame * (float)scarfy.width / 6;
+            frameRec.x = (float)currentFrame * (float)brownie_running.width / 6;
         }
 
         // Control frames speed
@@ -62,24 +66,26 @@ int main(void)
 
         ClearBackground(RAYWHITE);
 
-        DrawTexture(scarfy, 15, 40, WHITE);
-        DrawRectangleLines(15, 40, scarfy.width, scarfy.height, LIME);
-        DrawRectangleLines(15 + (int)frameRec.x, 40 + (int)frameRec.y, (int)frameRec.width, (int)frameRec.height, RED);
+        // DrawTexture(brownie, 15, 40, WHITE);
+        // DrawRectangleLines(15, 40, brownie.width, brownie.height, LIME);
+        // DrawRectangleLines(15 + (int)frameRec.x, 40 + (int)frameRec.y, (int)frameRec.width, (int)frameRec.height, RED);
 
-        DrawText("FRAME SPEED: ", 165, 210, 10, DARKGRAY);
-        DrawText(TextFormat("%02i FPS", framesSpeed), 575, 210, 10, DARKGRAY);
-        DrawText("PRESS RIGHT/LEFT KEYS to CHANGE SPEED!", 290, 240, 10, DARKGRAY);
+        // DrawText("FRAME SPEED: ", 165, 210, 10, DARKGRAY);
+        // DrawText(TextFormat("%02i FPS", framesSpeed), 575, 210, 10, DARKGRAY);
+        // DrawText("PRESS RIGHT/LEFT KEYS to CHANGE SPEED!", 290, 240, 10, DARKGRAY);
 
-        for (int i = 0; i < MAX_FRAME_SPEED; i++)
-        {
-            if (i < framesSpeed)
-                DrawRectangle(250 + 21 * i, 205, 20, 20, RED);
-            DrawRectangleLines(250 + 21 * i, 205, 20, 20, MAROON);
-        }
+        // for (int i = 0; i < MAX_FRAME_SPEED; i++)
+        // {
+        //     if (i < framesSpeed)
+        //         DrawRectangle(250 + 21 * i, 205, 20, 20, RED);
+        //     DrawRectangleLines(250 + 21 * i, 205, 20, 20, MAROON);
+        // }
 
-        DrawTextureRec(scarfy, frameRec, position, WHITE); // Draw part of the texture
+        DrawTextureRec(brownie_standing, static_rec, position_standing, WHITE);
 
-        DrawText("(c) Zachary Brownie sprite by Juho Antti Heinonen", screenWidth - 200, screenHeight - 20, 10, GRAY);
+        DrawTextureRec(brownie_running, frameRec, position, WHITE); // Draw part of the texture
+
+        DrawText("(c) Zachary Brownie sprite by Juho Antti Heinonen", screenWidth - 400, screenHeight - 20, 10, GRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -87,7 +93,7 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    UnloadTexture(scarfy); // Texture unloading
+    UnloadTexture(brownie_running); // Texture unloading
 
     CloseWindow(); // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
