cd2af56970b32c6bcd449af8672fbd79bbaf3df7
Juho Antti Heinonen <juho.antti.heinonen@gmail.com>
2025-09-16 19:12:12 +0300

    Continue

--- zacbrown.c
+++ zacbrown.c
@@ -1,11 +1,11 @@
 #include "raylib.h"
-//#include <stdio.h>
-#include "raymath.h"        // Required for: Vector2Clamp()
+// #include <stdio.h>
+#include "raymath.h" // Required for: Vector2Clamp()
 
 #include "custom_types.h"
 
-#define MAX(a, b) ((a)>(b)? (a) : (b))
-#define MIN(a, b) ((a)<(b)? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 #define MAX_FRAME_SPEED 15
 #define MIN_FRAME_SPEED 1
@@ -18,7 +18,7 @@ int main(void)
     const int screenHeight = 1600;
 
     // Enable config flags for resizable window and vertical synchro
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);    
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
     InitWindow((float)GetScreenWidth(), (float)GetScreenHeight(), "Zachary Brownie - sprite animation");
     SetWindowMinSize(320, 240);
 
@@ -27,10 +27,10 @@ int main(void)
 
     // Render texture initialization, used to hold the rendering result so we can easily resize it
     RenderTexture2D target = LoadRenderTexture(gameScreenWidth, gameScreenHeight);
-    SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);  // Texture scale filter to use    
+    SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR); // Texture scale filter to use
 
     // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
-    Texture2D brownie_standing = LoadTexture("img/brownie.png"); // Texture loading
+    Texture2D brownie_standing = LoadTexture("img/brownie.png");        // Texture loading
     Texture2D brownie_running = LoadTexture("img/brownie_running.png"); // Texture loading
 
     Vector2 position_standing = {350.0f, 150.0f};
@@ -41,13 +41,18 @@ int main(void)
     int currentFrame = 0;
 
     // initialize map here
-    game_tile game_map[20][10];
-    for (int x = 0; x < 20; x++) {
-        for (int y = 0; y < 10; y++) {
-            if (y == 0) {
-                game_map[x][y] = (game_tile){ .x = x, .y = y, .tile_type = BROWN_GROUND };
-            } else {
-                game_map[x][y] = (game_tile){ .x = x, .y = y, .tile_type = LIGHT_SKY };
+    game_tile game_map[20][8];
+    for (int x = 0; x < 20; x++)
+    {
+        for (int y = 0; y < 8; y++)
+        {
+            if (y == 7)
+            {
+                game_map[x][y] = (game_tile){.x = x, .y = y, .tile_type = BROWN_GROUND};
+            }
+            else
+            {
+                game_map[x][y] = (game_tile){.x = x, .y = y, .tile_type = LIGHT_SKY};
             }
         }
     }
@@ -61,7 +66,7 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
-        float scale = MIN((float)GetScreenWidth()/gameScreenWidth, (float)GetScreenHeight()/gameScreenHeight);
+        float scale = MIN((float)GetScreenWidth() / gameScreenWidth, (float)GetScreenHeight() / gameScreenHeight);
 
         // Update
         //----------------------------------------------------------------------------------
@@ -78,40 +83,39 @@ int main(void)
             frameRec.x = (float)currentFrame * (float)brownie_running.width / 6;
         }
 
-        // Control frames speed
-        if (IsKeyPressed(KEY_RIGHT))
-            framesSpeed++;
-        else if (IsKeyPressed(KEY_LEFT))
-            framesSpeed--;
-
-        if (framesSpeed > MAX_FRAME_SPEED)
-            framesSpeed = MAX_FRAME_SPEED;
-        else if (framesSpeed < MIN_FRAME_SPEED)
-            framesSpeed = MIN_FRAME_SPEED;
-        //----------------------------------------------------------------------------------
-
         // Draw
         //----------------------------------------------------------------------------------
         // Draw everything in the render texture, note this will not be rendered on screen, yet
-        BeginTextureMode(target);            
+        BeginTextureMode(target);
 
-            ClearBackground(RAYWHITE);
-            //DrawTextureRec(brownie_standing, static_rec, position_standing, WHITE);
+        ClearBackground(RAYWHITE);
 
-            DrawTextureRec(brownie_running, frameRec, position, WHITE); // Draw part of the texture
+        // Draw the map tiles
+        for (int x = 0; x < 20; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                Color tile_color = (game_map[x][y].tile_type == BROWN_GROUND) ? BROWN : (Color){173, 216, 230, 255}; // light blue
+                DrawRectangle(x * 64, y * 64, 64, 64, tile_color);
+            }
+        }
 
-            DrawText("(c) Zachary Brownie sprite by Juho Antti Heinonen", screenWidth - 400, screenHeight - 20, 10, GRAY);
+        // Draw the brownie above brown_ground (y=0)
+        DrawTextureRec(brownie_running, frameRec, (Vector2){position.x, 0}, WHITE);
+
+        DrawText("(c) Zachary Brownie sprite by Juho Antti Heinonen", screenWidth - 400, screenHeight - 20, 10, GRAY);
 
         EndTextureMode();
 
         BeginDrawing();
-            ClearBackground(BLACK);
-            DrawTexturePro(target.texture, (Rectangle){ 0.0f, 0.0f, (float)target.texture.width, (float)-target.texture.height },
-                           (Rectangle){ (GetScreenWidth() - ((float)gameScreenWidth*scale))*0.5f, (GetScreenHeight() - ((float)gameScreenHeight*scale))*0.5f,
-                           (float)gameScreenWidth*scale, (float)gameScreenHeight*scale }, (Vector2){ 0, 0 }, 0.0f, WHITE);
+        ClearBackground(BLACK);
+        DrawTexturePro(target.texture, (Rectangle){0.0f, 0.0f, (float)target.texture.width, (float)-target.texture.height},
+                       (Rectangle){(GetScreenWidth() - ((float)gameScreenWidth * scale)) * 0.5f, (GetScreenHeight() - ((float)gameScreenHeight * scale)) * 0.5f,
+                                   (float)gameScreenWidth * scale, (float)gameScreenHeight * scale},
+                       (Vector2){0, 0}, 0.0f, WHITE);
         EndDrawing();
 
-        //----------------------------------------------------------------------------------        
+        //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
